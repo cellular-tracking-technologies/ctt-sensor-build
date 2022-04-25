0443d13a9265b829285fc4da24a81fafe32adef3
Bob Fogg <bob.fogg@celltracktech.com>
2022-04-25 15:26:11 -0400

    update uploader and bash updater

--- roles/install-ctt-software/files/bash-update-station.sh
+++ roles/install-ctt-software/files/bash-update-station.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
-# https://unix.stackexchange.com/questions/48505/how-to-make-sure-only-one-instance-of-a-bash-script-runs
-LOCKDIR='/tmp/bash-update-station'
-function cleanup {
-    if rmdir $LOCKDIR; then
-        echo "Finished upload process"
-    else
-        echo "Failed to remove lock directory '$LOCKDIR'"
-        exit 1
-    fi
-}
-
-if mkdir $LOCKDIR; then
-    #Ensure that if we "grabbed a lock", we release it
-    #Works for SIGTERM and SIGINT(Ctrl-C)
-    trap "cleanup" EXIT
-
-    sudo /home/pi/ctt/station-utils/station-updater.py
-
-    # Processing starts here
+script='/home/ctt/ctt/station-utils/station-updater.py'
+processes=$(ps -ef | grep $script | grep -v grep | xargs)
+if [ -n "$processes" ]; then
+    echo 'killing prior upload process before beginning next upload session'
+    ps -ef | grep $script  | grep -v grep | awk '{print $2}' | sudo xargs kill
 else
-    echo "Could not create lock directory '$LOCKDIR'"
-    exit 1
+    echo 'no prior upload session'
 fi
+echo 'starting ctt data upload session'
+sudo $script
