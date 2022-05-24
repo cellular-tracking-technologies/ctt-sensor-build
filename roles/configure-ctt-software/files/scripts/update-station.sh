001827a4f49fb55608b4a068b2ef2d2b8648c3ab
Bob Fogg <bob.fogg@celltracktech.com>
2022-05-24 14:50:12 -0400

    install ctt software in /lib/ctt

--- roles/configure-ctt-software/files/scripts/update-station.sh
+++ roles/configure-ctt-software/files/scripts/update-station.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-home='/home/ctt/ctt'
-sudo -u ctt mkdir -p $home
+home='/lib/ctt'
+sudo mkdir -p $home
 cd $home
 git_url='https://github.com/cellular-tracking-technologies/sensor-station-software.git'
 
@@ -8,22 +8,27 @@ check_run() {
   echo "$changed_files" | grep --quiet "$1" && eval "$2"
 }
 
-dir='/home/ctt/ctt/sensor-station-software'
+sudo systemctl stop station-hardware-server
+sudo systemctl stop station-lcd-interface
+sudo systemctl stop station-radio-interface
+sudo systemctl stop station-web-interface
+
+dir="$home/sensor-station-software"
 # check if the software directory exists
 if [ -d $dir ]; then
   # directory exists - stash any changes and do a git pull
   cd $dir
-  sudo -u ctt git stash
-  sudo -u ctt git pull
+  sudo git stash
+  sudo ctt git pull
   # checking if package.json has changed
   changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)" 
-  check_run package.json "npm update && npm run build"
+  check_run package.json "npm update"
 else
   cd $home
   echo "cloning sensor-station-software repo to $dir"
-  sudo -u ctt git clone $git_url
+  sudo git clone $git_url
   cd $dir
-  npm update && npm run build
+  npm update
 fi
 
 sudo systemctl restart station-hardware-server
